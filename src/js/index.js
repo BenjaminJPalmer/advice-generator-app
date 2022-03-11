4c33d1442758a809018405217ab721c780eda47d
Benjamin Palmer <benpalmer13@hotmail.co.uk>
2022-03-11 15:16:35 +0000

    Added function to remove focus if not needed

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,15 @@ function newQuote() {
         });
     };
 
+function handleFirstTab(e) {
+    if (e.keyCode === 9) { // the "I am a keyboard user" key
+        document.body.classList.add('user-is-tabbing');
+        window.removeEventListener('keydown', handleFirstTab);
+    }
+}
+
+window.addEventListener('keydown', handleFirstTab);
+
 window.onload = () => {
     newQuote();
     };
