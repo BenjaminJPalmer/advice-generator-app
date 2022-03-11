8199c2ed297a73e10a6dc3d981da471c895f1ee0
Benjamin Palmer <benpalmer13@hotmail.co.uk>
2022-03-11 15:21:40 +0000

    Removed hover effect from mobile devices

--- src/js/index.js
+++ src/js/index.js
@@ -9,16 +9,7 @@ function newQuote() {
             advice.textContent = `"${data.slip.advice}"`;
         });
     };
-
-function handleFirstTab(e) {
-    if (e.keyCode === 9) { // the "I am a keyboard user" key
-        document.body.classList.add('user-is-tabbing');
-        window.removeEventListener('keydown', handleFirstTab);
-    }
-}
-
-window.addEventListener('keydown', handleFirstTab);
-
+    
 window.onload = () => {
     newQuote();
     };
