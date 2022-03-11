34e26b90ac4980f570b30c03cd41a58951482065
Benjamin Palmer <benpalmer13@hotmail.co.uk>
2022-03-11 13:36:31 +0000

    Fixed JS issue - script tag above html content

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const adviceId = document.getElementById("adviceId");
+const advice = document.getElementById("advice");
+
+function newQuote() { 
+    fetch("https://api.adviceslip.com/advice")
+        .then(response => response.json())
+        .then(data => {
+            adviceId.textContent = `${data.slip.id}`;
+            advice.textContent = `${data.slip.advice}`;
+        });
+    };
+
+window.onload = () => {
+    newQuote();
+    };
